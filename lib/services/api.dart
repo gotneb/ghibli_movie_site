61ca031e05eb68366ad4b5dc5cee4cc2184c5dc2
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-10-10 22:45:35 -0400

    fix: fecthing japanese title wrongly

---
+++ lib/services/api.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:ghibli_movie_site/models/movie.dart';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  static const path = 'studio-ghibli-movies-api.onrender.com';
+  static final dio = Dio();
+
+  static Future<List<Movie>> search({required String titleMovie}) async {
+    final url = '$path/movie/$titleMovie';
+    final response = await http.get(Uri.parse(url), headers: {
+      'Access-Control-Allow-Origin': '*',
+    });
+
+    //print('Response:\n${response.body}');
+    List<dynamic> json = jsonDecode(response.body);
+
+    final List<Movie> movies = [];
+    for (var data in json) {
+      try {
+        final movie = Movie.fromJson(data);
+        movies.add(movie);
+      } catch (e) {
+        print('Error while processing movie: $e');
+      }
+    }
+
+    return movies;
+  }
+
+  static Future<Movie> findByID({required String id}) async {
+    final url = '/movie/id/$id';
+    final response = await http.get(Uri.https(path, url));
+
+    // `utf8` decode is needed because japanese text is wrongly interpreted without it
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    final movie = Movie.fromJson(json);
+    return movie;
+  }
+}
