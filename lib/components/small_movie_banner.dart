cdfc7b54ef1d3dc02e753f5ac3704c4361f2e3b3
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-10-11 09:37:53 -0400

    wip: done basic design in below movies list

---
+++ lib/components/small_movie_banner.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class SmallMovieBanner extends StatefulWidget {
+  const SmallMovieBanner({super.key});
+
+  @override
+  State<SmallMovieBanner> createState() => _SmallMovieBannerState();
+}
+
+class _SmallMovieBannerState extends State<SmallMovieBanner> {
+  static const maxScale = 1.5;
+  static const normalScale = 1.0;
+
+  static const onEnterCurve = Curves.easeOutExpo;
+  static const onExitCurve = Curves.slowMiddle;
+
+  Curve curve = onEnterCurve;
+  double scale = 1.0;
+  double padding = 0.0;
+
+  bool isInside = false;
+
+  @override
+  Widget build(BuildContext context) {
+    const url =
+        'https://occ-0-3810-1740.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaPGYc3hhUZ8qw-OitkCVwU_Zr7IhWOhfFLx76osQUXxpt7bJuI6DV-fPpsLrN5-_39ufUQOzV11V0ilNSG2DRUnY4G_s5z23K1j.jpg?r=ac4';
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 400),
+      margin: EdgeInsets.symmetric(horizontal: isInside ? 50 : 0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: !isInside ? Colors.white : Colors.transparent,
+          width: !isInside ? 2 : 0,
+        ),
+      ),
+      child: AnimatedScale(
+        scale: scale,
+        duration: const Duration(seconds: 1),
+        curve: curve,
+        child: MouseRegion(
+          cursor:
+              isInside ? SystemMouseCursors.click : SystemMouseCursors.basic,
+          onEnter: onEnter,
+          onExit: onExit,
+          child: AspectRatio(
+            aspectRatio: 9 / 7,
+            child: Image.network(url, fit: BoxFit.fitHeight),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onEnter(PointerEnterEvent event) {
+    setState(() {
+      isInside = true;
+      scale = scale == normalScale ? maxScale : normalScale;
+      curve = curve == onEnterCurve ? onEnterCurve : onExitCurve;
+      padding = padding == 0.0 ? 30.0 : 0.0;
+    });
+  }
+
+  void onExit(PointerExitEvent event) {
+    setState(() {
+      isInside = false;
+      scale = scale == normalScale ? maxScale : normalScale;
+      curve = curve == onEnterCurve ? onEnterCurve : onExitCurve;
+      padding = padding == 0.0 ? 0.0 : 0.0;
+    });
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ghibli_movie_site/components/small_movie_banner.dart';
 import 'package:ghibli_movie_site/custom_widgets/custom_scroll.dart';
 import 'package:ghibli_movie_site/models/movie.dart';
 import 'package:ghibli_movie_site/services/api.dart';
@@ -9,7 +10,7 @@ class HomeView extends StatelessWidget {
 
   static const searchHeight = 65.0;
   static const mainContentRatio = 0.8;
-  static const sideContentRatio = 0.25;
+  static const sideContentRatio = 0.2;
   static const trailerRatio = 0.65;
 
   static const blackColor = Color(0xFF0D0D0D);
@@ -29,7 +30,7 @@ class HomeView extends StatelessWidget {
     );
 
     return FutureBuilder(
-      future: Api.search(titleMovie: 'whisper'),
+      future: Api.search(titleMovie: 'mononoke'),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return _buildBody(context, topMovie: snapshot.data![0]);
@@ -53,7 +54,7 @@ class HomeView extends StatelessWidget {
             colors: [blackColor, Colors.transparent],
             begin: Alignment.topLeft,
             end: Alignment.topRight,
-            stops: [1 - trailerRatio, .65]),
+            stops: [1 - trailerRatio, .70]),
       ),
     );
 
@@ -152,31 +153,30 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildMoviesList(BuildContext context) {
+
     final movies = ScrollConfiguration(
       behavior: MyCustomScrollBehavior(),
       child: ListView.separated(
         physics: const AlwaysScrollableScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemCount: 8,
-        separatorBuilder: (context, index) => const SizedBox(width: 60),
-        itemBuilder: (context, index) => AspectRatio(
-          aspectRatio: 5 / 4,
-          child: Container(color: Colors.white),
-        ),
+        itemCount: 6,
+        separatorBuilder: (context, index) => SizedBox(width: 0.02* MediaQuery.sizeOf(context).width),
+        itemBuilder: (context, index) => const SmallMovieBanner(),
       ),
     );
 
     return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: 0.04 * MediaQuery.sizeOf(context).width),
-      color: blackColor,
+      padding: EdgeInsets.symmetric(horizontal: 0.04 * MediaQuery.sizeOf(context).width),
+      decoration: BoxDecoration(
+        border: Border.all(width: 0, color: blackColor),
+        color: blackColor,
+      ),
       width: MediaQuery.sizeOf(context).width,
       //height: sideContentRatio * MediaQuery.sizeOf(context).height,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text('You might like', style: TextStyle(color: Colors.white)),
+        const Text('You might like', style: TextStyle(color: Colors.white)),
         const SizedBox(height: 16),
-        Container(
-          color: Colors.green,
+        SizedBox(
           height: sideContentRatio * MediaQuery.sizeOf(context).height,
           child: movies,
         ),
